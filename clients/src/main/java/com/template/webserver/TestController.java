f1b7e96478c3eafca6da1efd55e151f3bf9ddc05
JL47 <jorlopez@csumb.edu>
2021-03-19 18:09:02 -0700

    Issues with routing

--- clients/src/main/java/com/template/webserver/TestController.java
+++ clients/src/main/java/com/template/webserver/TestController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TestController {
@@ -18,7 +20,15 @@ public class TestController {
     }
 
     @GetMapping("/patientPage")
-    private String patientPage(){
+    private String patientPage(Model model){
+        model.addAttribute("userinfo",new userInfo());
+        return "patientPage";
+    }
+    @PostMapping("/patientPage")
+    private String loginPost(@ModelAttribute("userinfo") userInfo userinfo, Model model){
+        System.out.println("Username entered is: " + userinfo.getUsername());
+        System.out.println("Password entered is: " + userinfo.getPassword());
+
         return "patientPage";
     }
 
@@ -35,6 +45,27 @@ public class TestController {
     @GetMapping("/signUp")
     private String signUp () {
         return "signup";
+    }
+
+    public class userInfo {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
 

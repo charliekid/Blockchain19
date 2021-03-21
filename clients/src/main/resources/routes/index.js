75f181934a77d2e2985cee95ce44fe1bc8bc4a0a
Charlie Nguyen <charliekid19@gmail.com>
2021-03-21 13:15:19 -0700

    took out the .get

--- clients/src/main/resources/routes/index.js
+++ clients/src/main/resources/routes/index.js
@@ -5,14 +5,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var data;
-    http.get("http://localhost:10050/transaction/list", (resp) => {
-        resp.on("data", (information) => {
-            data += information
-        });
-        resp.on("end", () => {
-            console.log(data);
-        });
-    });
+    // http.get("http://localhost:10050/transaction/list", (resp) => {
+    //     resp.on("data", (information) => {
+    //         data += information
+    //     });
+    //     resp.on("end", () => {
+    //         console.log(data);
+    //     });
+    // });
     res.render('index', { title: 'Express' });
 });
 

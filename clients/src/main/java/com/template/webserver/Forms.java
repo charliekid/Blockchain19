1cbbbb4634743e57a455e1478befc166cec5d58a
Charlie Nguyen <charliekid19@gmail.com>
2021-03-23 11:19:17 -0700

    Retrospective form for post route

---
+++ clients/src/main/java/com/template/webserver/Forms.java
@@ -0,0 +1,33 @@
+package com.template.webserver;
+
+public class Forms {
+    public static class LoginForm {
+        String username;
+        String password;
+        String accountType;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getAccountType() {
+            return accountType;
+        }
+
+        public void setAccountType(String accountType) {
+            this.accountType = accountType;
+        }
+    }
+}
